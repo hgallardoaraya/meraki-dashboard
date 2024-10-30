db19b0c986e0ed8c0d23e38d7a47946689af82c3
Héctor Gallardo <hectorm.gallardo.araya@usach.cl>
2024-10-29 23:33:38 -0300

    template conexion a bdd

--- backend/go.mod
+++ backend/go.mod
@@ -16,14 +16,17 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.22.1 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
 	github.com/knz/go-libedit v1.10.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/libsql/sqlite-antlr4-parser v0.0.0-20240327125255-dbf53b6cbf06 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/tursodatabase/go-libsql v0.0.0-20241011135853-3effbb6dea5c // indirect
 	github.com/tursodatabase/libsql-client-go v0.0.0-20240902231107-85af5b9d094d // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
--- backend/main.go
+++ backend/main.go
@@ -4,19 +4,32 @@ import (
 	"database/sql"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 	"net/http"
 	"os"
 )
 
 func main() {
-	url := "libsql://[DATABASE].turso.io?authToken=[TOKEN]"
+	err := godotenv.Load()
+	if err != nil {
+		return
+	}
+	TursoDatabaseDns := os.Getenv("TURSO_DATABASE_DNS")
+	TursoAuthToken := os.Getenv("TURSO_AUTH_TOKEN")
+
+	fmt.Println("envs ", TursoDatabaseDns, TursoAuthToken)
+	url := fmt.Sprintf("libsql://%s.turso.io?authToken=%s", TursoDatabaseDns, TursoAuthToken)
 
 	db, err := sql.Open("libsql", url)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to open db %s: %s", url, err)
 		os.Exit(1)
+	} else {
+		fmt.Println("conexion exitosa a la base de datos")
 	}
 	defer db.Close()
+	queryUsers(db)
 
 	// Create a new router
 	r := gin.Default()
@@ -31,5 +44,37 @@ func main() {
 	// Start the server
 	if err := r.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server")
+	}
+}
+
+type User struct {
+	ID   int
+	Name string
+}
+
+func queryUsers(db *sql.DB) {
+	rows, err := db.Query("SELECT * FROM User;")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to execute query: %v\n", err)
+		os.Exit(1)
+	}
+	defer rows.Close()
+
+	var users []User
+
+	for rows.Next() {
+		var user User
+
+		if err := rows.Scan(&user.ID, &user.Name); err != nil {
+			fmt.Println("Error scanning row:", err)
+			return
+		}
+
+		users = append(users, user)
+		fmt.Println(user.ID, user.Name)
+	}
+
+	if err := rows.Err(); err != nil {
+		fmt.Println("Error during rows iteration:", err)
 	}
 }
